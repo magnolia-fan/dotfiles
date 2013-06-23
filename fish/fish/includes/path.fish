a4d88924d3ec5818f2e0ae86691705540c586a1b
Gregory Eremin <magnolia_fan@me.com>
2013-06-23 16:06:00 +0700

    More fish configuration

--- fish/fish/includes/path.fish
+++ fish/fish/includes/path.fish
@@ -1,10 +1,9 @@
+set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/sbin $PATH
+set -x PATH /usr/local/share/npm/bin $PATH
+set -x PATH /usr/local/share/python $PATH
 set -x PATH $HOME/.dotfiles/bin $PATH
+set -x PATH $HOME/.rbenv/plugins/ruby-build/bin $PATH
+set -x PATH $HOME/.rbenv/bin $PATH
 set -x PATH $HOME/.rbenv/shims $PATH
-set -x PATH /usr/local/share/npm/bin $PATH
-set -x PATH /usr/local/share/python $PATH
-set -x PATH ~/.dotfiles/bin $PATH
-set -x PATH /usr/local/share/npm/bin $PATH
-set -x PATH /usr/local/share/python $PATH
-set -x PATH ~/.misc/git-achievements $PATH
-set -x PATH /Applications/Sublime\ Text\ 3.app/Contents/SharedSupport/bin $PATH
+set -x PATH $HOME/.misc/git-achievements $PATH
