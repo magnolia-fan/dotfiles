afca6b7f6d707c07040074c5b15c807e9dfc196b
Gregory Eremin <g@erem.in>
2014-08-08 18:05:10 +0700

    Update path config

--- fish/fish/includes/path.fish
+++ fish/fish/includes/path.fish
@@ -1,8 +1,14 @@
 set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/sbin $PATH
 set -x PATH /usr/local/share/npm/bin $PATH
+
 set -x PATH $HOME/.dotfiles/bin $PATH
+
 set -x PATH $HOME/.rbenv/plugins/ruby-build/bin $PATH
 set -x PATH $HOME/.rbenv/bin $PATH
 set -x PATH $HOME/.rbenv/shims $PATH
+
 set -x PATH $HOME/.misc/git-achievements $PATH
+
+set -x PATH $HOME/go/bin $PATH
+# set -x PATH $HOME/Code/go/bin $PATH
