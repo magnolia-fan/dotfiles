762e7a9b41a573a84b670297b4da071e3e60426b
Gregory Eremin <g@erem.in>
2017-10-05 13:17:55 +0200

    d-ssh bash completion

--- bash/docker.bash
+++ bash/docker.bash
@@ -1,6 +1,11 @@
 function d-ssh {
     docker exec -it $1 /bin/bash
 }
+function _d-ssh {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=($(compgen -o nospace -W "$(docker ps -a --format '{{.Names}}')" -- $cur))
+}
+complete -F _d-ssh d-ssh
 
 function d-ps {
     docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Status}}\t{{.CreatedAt}}" $@
