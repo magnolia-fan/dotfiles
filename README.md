b78ac0666b3d9895de0761a8fcdbc3de0ee3e6ff
Gregory Eremin <root@localhost>
2020-09-12 10:10:56 +0200

    Add license

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 git clone git@github.com:localhots/dotfiles.git ~/dotfiles
 ~/dotfiles/setup.bash
 ```
+
+### Changelog
+
+* [Tree](https://github.com/localhots/dotfiles/tree/2f17a9f7300b61a382ec3dfeb69de648b3d36b43) before purging macOS and fish configs.
--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/bash/bashrc.sh ~/.bashrc
+
 # Git
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/git/gitignore ~/.gitignore
@@ -8,5 +12,8 @@ ln -s ~/dotfiles/ruby/irbrc ~/.irbrc
 ln -s ~/dotfiles/ruby/aprc ~/.aprc
 
 # VIM
-ln -s ~/dotfiles/vim/vim ~/.vim
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+# These configs are old and not maintained, don't link them automatically
+# ln -s ~/dotfiles/vim/vim ~/.vim
+# ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+
+
