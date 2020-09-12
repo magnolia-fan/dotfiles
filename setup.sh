3719520240302239e7c675ce17ea5b397138bc46
Gregory Eremin <root@localhost>
2020-09-12 09:56:11 +0200

    Purge remaining macos stuff

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# Git
+ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/gitignore ~/.gitignore
+
+# Ruby
+ln -s ~/dotfiles/ruby/gemrc ~/.gemrc
+ln -s ~/dotfiles/ruby/irbrc ~/.irbrc
+ln -s ~/dotfiles/ruby/aprc ~/.aprc
+
+# VIM
+ln -s ~/dotfiles/vim/vim ~/.vim
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
